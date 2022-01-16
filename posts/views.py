b9e688268f62f81a61c85fc663a6e9a615d22c8f
Oscar Rodriguez <rrodriguezsame@gmail.com>
2022-01-16 15:32:42 -0800

    added features to my blog

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
+
+class PostListView(ListView):
+    template_name = "blog/list.html"
+    model = Post
+
+
+class PostDetailView(DetailView):
+    template_name = "blog/detail.html"
+    model = Post 
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "blog/new.html"
+    model = Post
+    fields = ["title", "author", "body"]
